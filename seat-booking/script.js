82559674d7bad4e58a921fdbac323ffdca806138
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-03 18:38:09 +0430

    add initial javascript file

---
+++ seat-booking/script.js
@@ -0,0 +1,9 @@
+const movieSelect = document.getElementById('movie');
+const seat = document.querySelectorAll('.row .seat:not(.sold)');
+const proceed = document.getElementsByTagName('button');
+const seatsNumber = document.getElementById('number');
+const totalPrice = document.getElementById('total');
+const ticketPrice = +movieSelect.value;
+
+//-----------------------------------------------------
+seat.addEventListener('', function () {});
