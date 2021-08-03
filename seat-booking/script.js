170897075bc6fb9a02b070714ac5972c845226d0
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-03 19:28:59 +0430

    add functions: select seats and calculate total price

--- seat-booking/script.js
+++ seat-booking/script.js
@@ -1,9 +1,34 @@
+const seatsContainer = document.querySelector('.seat-container');
 const movieSelect = document.getElementById('movie');
 const seat = document.querySelectorAll('.row .seat:not(.sold)');
 const proceed = document.getElementsByTagName('button');
 const seatsNumber = document.getElementById('number');
 const totalPrice = document.getElementById('total');
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //-----------------------------------------------------
-seat.addEventListener('', function () {});
+function updateNumberAndPrice() {
+  const selectedSeats = document.querySelectorAll('.row .selected');
+  const selectedSeatsNumber = selectedSeats.length;
+  seatsNumber.innerText = selectedSeatsNumber;
+  totalPrice.innerText = selectedSeatsNumber * ticketPrice;
+}
+//-----------------------------------------------------
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  console.log(ticketPrice);
+  updateNumberAndPrice();
+});
+
+//-----------------------------------------------------
+
+seatsContainer.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('sold')
+  ) {
+    e.target.classList.toggle('selected');
+  }
+
+  updateNumberAndPrice();
+});
