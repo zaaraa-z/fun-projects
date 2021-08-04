ded1ca83e94bca506f8e28a8a44d5f20ed9b4f97
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-04 13:57:31 +0430

    update functionalities: localStorage

--- seat-booking/script.js
+++ seat-booking/script.js
@@ -1,23 +1,39 @@
 const seatsContainer = document.querySelector('.seat-container');
 const movieSelect = document.getElementById('movie');
-const seat = document.querySelectorAll('.row .seat:not(.sold)');
+const seats = document.querySelectorAll('.row .seat:not(.sold)');
 const proceed = document.getElementsByTagName('button');
 const seatsNumber = document.getElementById('number');
 const totalPrice = document.getElementById('total');
 let ticketPrice = +movieSelect.value;
 
 //-----------------------------------------------------
+function storeMovieData(movieIndex, movieTicketPrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMovieTicketPrice', movieTicketPrice);
+}
+
 function updateNumberAndPrice() {
   const selectedSeats = document.querySelectorAll('.row .selected');
+  const selectedSeatsIndexArr = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  }); //tip
+
+  localStorage.setItem(
+    'selectedSeatsIndex',
+    JSON.stringify(selectedSeatsIndexArr)
+  ); //tip
+
   const selectedSeatsNumber = selectedSeats.length;
   seatsNumber.innerText = selectedSeatsNumber;
   totalPrice.innerText = selectedSeatsNumber * ticketPrice;
 }
 //-----------------------------------------------------
+
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
-  console.log(ticketPrice);
   updateNumberAndPrice();
+
+  storeMovieData(e.target.selectedIndex, e.target.value); //tip
 });
 
 //-----------------------------------------------------
