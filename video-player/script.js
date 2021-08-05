35935a783a66f9d5d08e418317a135764e42495a
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-05 18:49:24 +0430

    update final files

--- video-player/script.js
+++ video-player/script.js
@@ -1,25 +1,58 @@
-const PlayBtn = document.getElementById('play');
+const playBtn = document.getElementById('play');
 const stopBtn = document.getElementById('stop');
-const screen = document.getElementsByTagName('video');
+const video = document.getElementById('video');
 const progressbar = document.getElementById('progressbar');
 const timestamp = document.getElementById('timestamp');
 
 //------------------------------------------------------------------------
-toggleVideoStatus() {
-
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
+function updatePlayIcon() {
+  if (video.paused) {
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  } else {
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+}
 
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+function updateProgressbar() {
+  progressbar.value = (video.currentTime * 100) / video.duration;
+
+  //set the time
+  let min = Math.floor(video.currentTime / 60);
+  let sec = Math.floor(video.currentTime - min * 60);
+  if (min < 10) {
+    min = '0' + String(min);
+  }
+  if (sec < 10) {
+    sec = '0' + String(sec);
+  }
+  timestamp.innerText = `${min}:${sec}`;
+}
+
+function dragProgressbar() {
+  video.currentTime = (+progressbar.value * video.duration) / 100;
+}
 //------------------------------------------------------------------------
 
 playBtn.addEventListener('click', toggleVideoStatus);
-screen.addEventListener('click', toggleVideoStatus);
+video.addEventListener('click', toggleVideoStatus);
 
-screen.addEventListener('pause', updatePlayIcon);
-screen.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
 
 stopBtn.addEventListener('click', stopVideo);
 
-progressbar.addEventListener('change', setProgressbar)
-screen.addEventListener('timeupdate', updateProgressbar);
-
+video.addEventListener('timeupdate', updateProgressbar);
+progressbar.addEventListener('change', dragProgressbar);
