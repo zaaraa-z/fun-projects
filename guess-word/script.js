ab4e72eb59a1de054994efba9fe23bbf5c2402d7
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-15 21:57:35 +0430

    update final files: complete all js functions

--- guess-word/script.js
+++ guess-word/script.js
@@ -1,4 +1,4 @@
-const petal = document.querySelectorAll('.petal');
+const petals = document.querySelectorAll('.petal');
 const letters = document.getElementById('letters');
 const wrong = document.getElementById('wrong');
 const finalPopup = document.getElementById('final-popup-container');
@@ -24,8 +24,7 @@ const wordArr = [
 
 let selectedWord = wordArr[Math.floor(Math.random() * wordArr.length)];
 
-// correctLetters = selectedWord.split('');
-let correctLetters = ['n', 'i', 't', 'g', 'r', 't', 'y', 'e'];
+let correctLetters = [];
 let wrongLetters = [];
 
 function placeWord() {
@@ -55,8 +54,64 @@ window.addEventListener('keydown', (e) => {
       } else {
         showNotification();
       }
+    } else {
+      if (!wrongLetters.includes(keyboardLetter)) {
+        wrongLetters.push(keyboardLetter);
+        updateWrongSec();
+      } else {
+        showNotification();
+      }
     }
   }
 });
 
+//-----------------------------------------
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2500);
+}
+
+//-----------------------------------------
+function updateWrongSec() {
+  //show wrong letters
+  if (wrongLetters.length > 0) {
+    wrong.innerHTML = `<div class="wrong-info-container"><p>WRONG...</p>
+    <span>${wrongLetters}</span></div>`;
+  } else {
+    wrong.innerHTML = '';
+  }
+
+  if (wrongLetters.length >= 7) {
+    //check lost
+    finalPopupMessage.innerText = "Oh! My Poor Flower...It's OK :)";
+    finalPopup.style.display = 'flex';
+  }
+
+  //flower
+  petals.forEach((petal, index) => {
+    const currentWrongLettersNo = wrongLetters.length;
+    if (index < currentWrongLettersNo) {
+      petal.classList.add('drop');
+    } else {
+      petal.classList.remove('drop');
+    }
+  });
+}
+
+//-----------------------------------------
+playAgainBtn.addEventListener('click', () => {
+  finalPopup.style.display = 'none';
+
+  wrongLetters.splice(0);
+  correctLetters.splice(0);
+  selectedWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+  // wrong.innerHTML = '';
+  updateWrongSec();
+  placeWord();
+});
+
+//-----------------------------------------
 placeWord();
