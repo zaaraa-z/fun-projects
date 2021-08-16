3f07ca89efa5572ab9a0c9f2d45f99cdd1accbca
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-16 23:41:32 +0430

    update css file & add functions to js file

---
+++ meals-to-rescue/script.js
@@ -0,0 +1,50 @@
+const searchInput = document.getElementById('search-input'),
+  submitSearch = document.getElementById('submit-search'), //tip: submit event only works on form element
+  randomBtn = document.getElementById('random-btn'),
+  resultHeading = document.getElementById('result-heading'),
+  resultMeals = document.getElementById('result-meals'),
+  resultSinglemeal = document.getElementById('result-single-meal');
+
+//------------------------------------
+function searchMeals(e) {
+  e.preventDefault();
+
+  //clear previous searched items on screen
+  resultMeals.innerHTML = '';
+
+  let searchedWord = searchInput.value;
+  if (searchedWord.trim()) {
+    fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${searchedWord}
+    `)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h3>Search Results for "${searchedWord}":</h3>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML =
+            '<h3>No meal has found! Please try again</h3>';
+        } else {
+          resultMeals.innerHTML = data.meals
+            .map(
+              (meal) =>
+                `<div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-title" data-mealID="${meal.idMeal}">
+                <h4>${meal.strMeal}</h4>
+              </div>
+            </div>`
+            )
+            .join('');
+        }
+      });
+  } else {
+    alert('Please enter a search word');
+  }
+}
+
+function findRandomMeal() {}
+
+//------------------------------------
+submitSearch.addEventListener('submit', searchMeals);
+randomBtn.addEventListener('click', findRandomMeal);
