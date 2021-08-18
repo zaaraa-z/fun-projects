7f7f7dd190247538149602ef4c3b2313643364f4
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-18 23:53:50 +0430

    update js file by adding 80% of functions

--- expense-review/script.js
+++ expense-review/script.js
@@ -17,6 +17,34 @@ const initialTransactions = [
 const transactionItems = initialTransactions;
 
 //------------------------------------------------------
+function addNewTransaction(e) {
+  e.preventDefault();
+
+  const enteredItem = itemInput.value.trim();
+  const enteredAmount = +amoutInput.value.trim();
+
+  if (enteredAmount === '' || enteredItem === '') {
+    alert('"Item" & "Amount" is required!');
+  } else {
+    const newTransaction = {
+      id: createID(),
+      text: enteredItem,
+      amount: enteredAmount,
+    };
+
+    transactionItems.push(newTransaction);
+
+    displayTransactions(newTransaction);
+    updateTotals();
+    itemInput.value = '';
+    amoutInput.value = '';
+  }
+}
+//-----------------
+function createID() {
+  return Math.floor(Math.random() * 1000);
+}
+//-----------------
 function displayTransactions(item) {
   //identify that it is expense (+) or income (-)
   const moneySign = item.amount > 0 ? '+' : '-';
@@ -25,14 +53,51 @@ function displayTransactions(item) {
   liEl.classList.add(item.amount < 0 ? 'expense' : 'income');
 
   liEl.innerHTML = `
-  ${item.text}<span>${moneySign}${Math.abs(item.amount)}</span>
-  <button id="del-btn">x</button>
+  ${item.text}<span>${moneySign} $${Math.abs(item.amount)}</span>
+  <button id="del-btn" onclick="deleteTransaction()">x</button>
   </li>
   `;
 
   historyList.appendChild(liEl);
 }
+//-----------------
+function updateTotals() {
+  //get all amounts
+  const amountsArr = transactionItems.map((x) => x.amount);
 
+  console.log(amountsArr);
+
+  //calculate expenses
+  moneyExpense.innerText =
+    '$' +
+    Math.abs(
+      amountsArr
+        .filter((item) => item < 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2)
+    );
+
+  //calculate incomes
+  moneyIncome.innerText =
+    '$' +
+    amountsArr
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+
+  //calculate total balance
+  balance.innerText =
+    '$' + amountsArr.reduce((acc, item) => (acc += item), 0).toFixed(2);
+}
+//-----------------
+function deleteTransaction() {}
 //------------------------------------------------------
-// add.addEventListener('submit', addAndDisplayTransactions);
-// deleteBtn.addEventListener('click', deleteTransaction);
+add.addEventListener('submit', addNewTransaction);
+
+function init() {
+  historyList.innerHTML = '';
+  transactionItems.forEach(displayTransactions);
+  updateTotals();
+}
+
+init();
