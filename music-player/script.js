246fad470b82baed34e4f10efa3bf4bfd0e882cf
zaaraa-z <zamanian.zahra@gmail.com>
2021-08-20 21:14:41 +0430

    update js functions: play, next & previous btns

---
+++ music-player/script.js
@@ -0,0 +1,66 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+const progressbar = document.getElementById('progressbar');
+const progressbarContainer = document.getElementById('progressbar-container');
+const musicTitle = document.getElementById('music-title');
+const musicCover = document.getElementById('music-cover');
+const audio = document.getElementById('audio');
+
+const musicsArr = ['energetic', 'relax', 'happy'];
+let musicIndex = 1;
+//---------------------------------------------------------------
+loadMusic(musicsArr[musicIndex]);
+
+//---------------------------------------------------------------
+function loadMusic(music) {
+  musicTitle.innerText = music;
+  audio.src = `music/${music}.mp3`;
+  musicCover.src = `img/${music}.jpg`;
+}
+
+//-----------------------
+function playMusic() {
+  musicContainer.classList.add('play');
+  playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
+  audio.play();
+}
+
+//-----------------------
+function pauseMusic() {
+  musicContainer.classList.remove('play');
+  playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+  audio.pause();
+}
+
+//-----------------------
+function nextMusic() {
+  musicIndex++;
+  if (musicIndex > musicsArr.length - 1) {
+    musicIndex = 0;
+  }
+  loadMusic(musicsArr[musicIndex]);
+  playMusic();
+}
+
+//-----------------------
+function prevMusic() {
+  musicIndex--;
+  if (musicIndex < 0) {
+    musicIndex = musicsArr.length - 1;
+  }
+  loadMusic(musicsArr[musicIndex]);
+  playMusic();
+}
+
+//---------------------------------------------------------------
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+  isPlaying ? pauseMusic() : playMusic();
+});
+
+nextBtn.addEventListener('click', nextMusic);
+prevBtn.addEventListener('click', prevMusic);
+
+progressbar.addEventListener()
