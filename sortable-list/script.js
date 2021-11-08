92e26bfbc289c1161afb3e88529d776d4c813fb9
zaaraa-z <zamanian.zahra@gmail.com>
2021-11-08 23:34:12 +0330

    add initial variables in js

---
+++ sortable-list/script.js
@@ -0,0 +1,14 @@
+const list = document.getElementById('sortable-list');
+const checkBtn = document.getElementById('check-btn');
+const biggestStates = [
+  'Alaska',
+  'Texas',
+  'California',
+  'Montana',
+  'New Mexico',
+  'Arizona',
+  'Nevada',
+  'Colorado',
+  'Oregon',
+  'Wyoming',
+];
