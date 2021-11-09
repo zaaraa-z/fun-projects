ee84a1106d95d3c5eb00f1066b775bb7368a932b
zaaraa-z <zamanian.zahra@gmail.com>
2021-11-09 11:47:06 +0330

    create list in correct order in DOM

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -1,5 +1,7 @@
 const list = document.getElementById('sortable-list');
 const checkBtn = document.getElementById('check-btn');
+
+//the main list in correct order
 const biggestStates = [
   'Alaska',
   'Texas',
@@ -12,3 +14,30 @@ const biggestStates = [
   'Oregon',
   'Wyoming',
 ];
+
+//the displaying list in DOM
+const listArr = [];
+// let index;
+
+//------------------------------------------------------
+function createList() {
+  [...biggestStates].forEach((USState, index) => {
+    const liEl = document.createElement('li');
+    liEl.setAttribute('data-index', index);
+
+    liEl.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+            <p class="state-name">${USState}</p>
+            <i class="fas fa-arrows-alt"></i>
+        </div>
+    `;
+
+    listArr.push(liEl);
+    list.appendChild(liEl);
+  });
+}
+
+//------------------------------------------------------
+createList();
+console.log(listArr);
