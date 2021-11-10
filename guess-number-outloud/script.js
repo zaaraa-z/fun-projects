13d2bd50746d13b6d9fd7f7215df9bd155779d58
zaaraa-z <zamanian.zahra@gmail.com>
2021-11-10 18:45:13 +0330

    add checkWord function

--- guess-number-outloud/script.js
+++ guess-number-outloud/script.js
@@ -32,5 +32,36 @@ function showMessage(word) {
     `;
 }
 
+//check the word and show the coresponding message
+function checkWord(word) {
+  const wordToNumber = +word;
+  let guideMessage;
+
+  if (Number.isNaN(wordToNumber)) {
+    message.innerHTML = `<p>That's not a number...</p>`;
+    return;
+  }
+
+  if (wordToNumber > 100 || wordToNumber < 1) {
+    message.innerHTML = `<p>Between 1 & 100 please...</p>`;
+    return;
+  }
+
+  if (wordToNumber > randomNumber) {
+    message.innerHTML += `<p>Go lower!<p>`;
+  } else if (wordToNumber < randomNumber) {
+    message.innerHTML += `<p>Go higher!<p>`;
+  } else {
+    document.body.innerHTML = `
+    <h1>
+        Yes!!! You Guessed The Word! <br>
+        <h3>It Was ${wordToNumber}</h3>
+    </h1>
+    <button class="play-again" id="play-again">Play Again</button>
+    `;
+  }
+}
+
 //----------------------event listeners----------------------
 recognition.addEventListener('result', captureWord);
+recognition.addEventListener('end', () => recognition.start());
